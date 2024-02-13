135eb4b80869a68fadcfd30ca350a9a7eb00aa10
yuval <yuvalchayko@gmail.com>
2024-02-13 18:54:53 +0200

    data base

--- joined_calendar_db.py
+++ joined_calendar_db.py
@@ -35,9 +35,9 @@ class joined_calendar_db:
                 "CREATE TABLE IF NOT EXISTS " + self.calendars_participants +
                 " (calendar_id VARCHAR(3), participant VARCHAR(20), color VARCHAR(20), PRIMARY KEY(calendar_id, participant))",
                 "CREATE TABLE IF NOT EXISTS " + self.events_participants +
-                " (event_id VARCHAR(4), participant VARCHAR(20), PRIMARY KEY(event_id))",
+                " (event_id VARCHAR(4), participant VARCHAR(20), PRIMARY KEY(event_id, participant))",
                 "CREATE TABLE IF NOT EXISTS " + self.event_info +
-                " (event_id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, calendar_id VARCHAR(3), name VARCHAR(30), manager VARCHAR(20), start_hour VARCHAR(5), end_hour VARCHAR(5), date VARCHAR(10), PRIMARY KEY(event_id))",
+                " (event_id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, calendar_id VARCHAR(3), name VARCHAR(30), manager VARCHAR(20), start_hour VARCHAR(5), end_hour VARCHAR(5), date VARCHAR(10))",
                 "CREATE TABLE IF NOT EXISTS " + self.calendar_invitations +
                 " (username VARCHAR(20), invited_by VARCHAR(20), calendar_id VARCHAR(3), PRIMARY KEY(username, invited_by, calendar_id))",
                 "CREATE TABLE IF NOT EXISTS " + self.event_invitations +
@@ -91,17 +91,17 @@ class joined_calendar_db:
         :param manager: str
         :return: id(str) or empty str if already exists
         """
-        #if not self._is_calendar_exists(self.last_calendar_id):
         sql_table = "INSERT INTO " + self.calendars + "(name,manager)  VALUES(?,?)"
         self.db_cursor.execute(sql_table, (name, manager,))
         self.db_conn.commit()
+
         "add manager to participant table"
 
         sql = "SELECT calendar_id from " + self.calendars + " ORDER BY calendar_id DESC LIMIT (1)"
         self.db_cursor.execute(sql)
-        self.add_calendar_participant(self.db_cursor.fetchone()[0], manager)
-        #else:
-        #    print("error! calendar already exists")
+        id = self.db_cursor.fetchone()[0]
+        self.add_calendar_participant(id, manager)
+        return id
 
 
     def add_calendar_participant(self, id, user):
@@ -111,6 +111,7 @@ class joined_calendar_db:
         :param user: str
         :return: None
         """
+        flag = False
         if self._is_calendar_exists(id):
 
             # check if participant is not already exists
@@ -119,10 +120,12 @@ class joined_calendar_db:
                 sql_table = "INSERT INTO " + self.calendars_participants + " VALUES(?,?,?)"
                 self.db_cursor.execute(sql_table, (id, user, self.find_color(id),))
                 self.db_conn.commit()
+                flag = True
             else:
                 print("already is a participant")
         else:
             print("calendar do not exists so cant add participant")
+        return flag
 
     def find_color(self, id):
         """
@@ -134,11 +137,12 @@ class joined_calendar_db:
         self.db_cursor.execute(sql_table, (id,))
         current_colors = self.db_cursor.fetchall()
         current_colors = [i[0] for i in current_colors]
-
+        color = ""
         for c in self.colors:
             if c not in current_colors:
-                return c
-        return ""
+                color = c
+                break
+        return color
 
     def add_user(self, username, password, phone):
         """
@@ -181,15 +185,18 @@ class joined_calendar_db:
         :param invited:
         :return:
         """
+        flag = False
         if self._is_calendar_exists(id) and self._is_user_exists(invited) and self._is_user_exists(invited_by):
             if not self.is_calendar_invitation_exists(id, invited):
                 sql_table = "INSERT INTO " + self.calendar_invitations + " VALUES(?,?,?)"
                 self.db_cursor.execute(sql_table, (invited, invited_by, id,))
                 self.db_conn.commit()
+                flag = True
             else:
                 print("invitation already exists")
         else:
             print("the calendar doesnt exists so cant add invitation or user not exists")
+        return flag
 
     def is_calendar_invitation_exists(self, id, user):
         """
@@ -205,9 +212,10 @@ class joined_calendar_db:
 
         return user in invitations
 
-    def add_event(self,name, participants, calendar_id, manager, start, end, date):
+    def add_event(self, name, participants, calendar_id, manager, start, end, date):
         """
         add event to the table and add the invitations to the invitations table if time available
+        :param name: str
         :param participants: list of participants
         :param calendar_id:
         :param manager:
@@ -216,7 +224,7 @@ class joined_calendar_db:
         :param date:
         :return:
         """
-
+        event_id = ""
         if self._is_calendar_exists(calendar_id):
 
             if self.is_participants_in_calendar(participants, calendar_id):
@@ -226,13 +234,15 @@ class joined_calendar_db:
                     self.db_conn.commit()
                     sql = "SELECT event_id from " + self.event_info + " ORDER BY event_id DESC LIMIT (1)"
                     self.db_cursor.execute(sql)
-                    self.add_event_participant(self.db_cursor.fetchone()[0], manager)
+                    event_id = self.db_cursor.fetchone()[0]
+                    self.add_event_participant(event_id, manager)
                 else:
                     print("time is not available")
             else:
                 print("errorrr event participants are not calendar participants")
         else:
             print("calendar do not exists so cant add event")
+        return event_id
 
     def add_event_participant(self, id, participant):
         """
@@ -241,7 +251,21 @@ class joined_calendar_db:
         :param participant:
         :return:
         """
-        pass
+        flag = False
+        if self._is_event_exists(id):
+
+            # check if participant is not already exists
+            if not self.is_participant_exists_in_event(id, participant):
+
+                sql_table = "INSERT INTO " + self.events_participants + " VALUES(?,?)"
+                self.db_cursor.execute(sql_table, (id, participant,))
+                self.db_conn.commit()
+                flag = True
+            else:
+                print("errorrr already is a participant")
+        else:
+            print("event do not exists so cant add participant")
+        return flag
 
     def is_event_exists(self, id):
         """
@@ -249,7 +273,10 @@ class joined_calendar_db:
         :param id:
         :return:
         """
-        pass
+        sql = "SELECT event_id FROM " + self.event_info + " WHERE event_id = ?"
+
+        self.db_cursor.execute(sql, (id,))
+        return self.db_cursor.fetchone() is not None
 
     def is_participant_exists_in_event(self, id, username):
         """
@@ -258,7 +285,12 @@ class joined_calendar_db:
         :param username:
         :return:
         """
-        pass
+        sql_table = "SELECT participant FROM " + self.events_participants + " WHERE event_id = ?"
+        self.db_cursor.execute(sql_table, (id,))
+        participants = self.db_cursor.fetchall()
+        participants = [i[0] for i in participants]
+
+        return username in participants
 
     def get_calendar_participants(self, id):
         """
@@ -279,11 +311,13 @@ class joined_calendar_db:
         :param id:
         :return: bool
         """
+        flag = True
         calendar_participants = self.get_calendar_participants(id)
         for x in participants:
             if not x in calendar_participants:
-                return False
-        return True
+                flag = False
+                break
+        return flag
 
     def check_is_time_available(self, username, start, end, date):
         """
@@ -294,7 +328,7 @@ class joined_calendar_db:
         :param date:
         :return: bool
         """
-
+        flag = False
         if self._is_user_exists(username):
             sql = "SELECT start_hour, end_hour FROM " + self.event_info + " WHERE manager = ? AND date = ?"
             self.db_cursor.execute(sql, (username, date,))
@@ -304,12 +338,120 @@ class joined_calendar_db:
             end = int(end.replace(":", ""))
             for x in hours:
                 if (start >= int(x[0].replace(":", "")) and start < int(x[1].replace(":", ""))) or (end > int(x[0].replace(":", "")) and end <= int(x[1].replace(":", ""))):
-                    return False
-            return True
+                    break
+            else:
+                flag = True
         else:
             print("user do not exists so cant check time availability")
-            return False
+        return flag
 
+    def add_event_invitation(self, event_id, calendar_id, invited, invited_by):
+        """
+
+        :param event_id:
+        :param calendar_id:
+        :param invited:
+        :param invited_by:
+        :return:
+        """
+        flag = False
+        if self._is_event_exists(event_id) and self._is_user_exists(invited) and self._is_user_exists(invited_by):
+            if not self.is_event_invitation_exists(event_id, invited) and self.is_participant_exists_in_calendar(calendar_id, invited):
+                sql_table = "INSERT INTO " + self.event_invitations + " VALUES(?,?,?,?)"
+                self.db_cursor.execute(sql_table, (invited, invited_by, calendar_id, event_id,))
+                self.db_conn.commit()
+                flag = True
+            else:
+                print("invitation already exists")
+        else:
+            print("the calendar doesnt exists so cant add invitation or user not exists")
+        return flag
+
+    def is_event_invitation_exists(self, event_id, invited):
+        """
+        check if invitation already exists
+        :param event_id:
+        :param invited:
+        :return:
+        """
+        sql_table = "SELECT username FROM " + self.event_invitations + " WHERE event_id = ?"
+        self.db_cursor.execute(sql_table, (id,))
+        invitations = self.db_cursor.fetchall()
+        invitations = [i[0] for i in invitations]
+
+        return invited in invitations
+
+    def is_manager_calander(self, id, user):
+        """
+        check if user is manager
+        :param id:
+        :param user:
+        :return:
+        """
+        sql = "SELECT manager FROM " + self.calendars + " WHERE calendar_id = ?"
+        self.db_cursor.execute(sql, (id,))
+        manager = self.db_cursor.fetchone()
+
+        return manager == user
+
+    def is_manager_event(self, id, user):
+        """
+        check if user is manager
+        :param id:
+        :param user:
+        :return:
+        """
+        sql = "SELECT manager FROM " + self.events_participants + " WHERE event_id = ?"
+        self.db_cursor.execute(sql, (id,))
+        manager = self.db_cursor.fetchone()
+
+        return manager == user
+
+    def get_password(self, username):
+        """
+        return password
+        :param username:
+        :return:
+        """
+
+        sql = "SELECT password FROM " + self.users + " WHERE username = ?"
+        self.db_cursor.execute(sql, (username,))
+        password = self.db_cursor.fetchone()
+        if password == None:
+            password = "-1"
+        return password
+
+    def change_calendar_name(self, id, name):
+        """
+        change the calendar name
+        :param id:
+        :param name:
+        :return:
+        """
+        sql = "UPDATE " + self.calendars + " SET name = " + name + " WHERE calendar_id = ?"
+        self.db_cursor.execute(sql, (id,))
+
+    def change_event_name(self, id, name):
+        """
+           change the event name
+           :param id:
+           :param name:
+           :return:
+           """
+        sql = "UPDATE " + self.event_info + " SET name = " + name + " WHERE event_id = ?"
+        self.db_cursor.execute(sql, (id,))
+
+    def change_event_time(self, id, start, end, date):
+        """
+        change event time
+        :param id:
+        :param start:
+        :param end:
+        :param date:
+        :return:
+        """
+        sql = "UPDATE " + self.event_info + " SET start_hour = " + start + " , end_hour = " + end + " , date = " + date + " WHERE event_id = ?"
+        self.db_cursor.execute(sql, (id,))
 
 if __name__ == '__main__':
     db = joined_calendar_db()
@@ -320,10 +462,11 @@ if __name__ == '__main__':
     print(db.add_calendar_participant("1", "test2"))
     print(db.add_calendar_participant("1", "test2"))
     print(db.add_calendar_invitation("1", "test1", "test2"))
-    print(db.add_calendar_invitation("1", "test3", "test2"))
-    print("here")
+    print(db.add_calendar_invitation("1", "fake", "test2"))
     print(db.add_event("test name", ["test1", "test2"], "1", "test1", "14:00", "16:00", "12.2.2024"))
-
+    print(db.add_event_participant("1", "test1"))
+    print(db.add_event_participant("1", "test2"))
+    print(db.add_event_participant("700", "test3"))
 
     #print(db.find_color("100"))
 
