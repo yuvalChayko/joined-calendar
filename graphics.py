7a2609c954e9a56b610e38861a7636c37b76018d
yuvalChayko <150593875+yuvalChayko@users.noreply.github.com>
2024-04-08 15:26:03 +0300

    Add files via upload

--- graphics.py
+++ graphics.py
@@ -49,7 +49,7 @@ class MainPanel(wx.Panel):
         # The first panel to show
         self.SetSizer(v_box)
         self.Layout()
-        self.first.Show()
+        self.calendar.Show()
 
 
     def listen(self, msg, param=None):
@@ -71,35 +71,68 @@ class LoginPanel(wx.Panel):
         sizer = wx.BoxSizer(wx.VERTICAL)
         # title
         title = wx.StaticText(self,-1, label="Login")
-        titlefont = wx.Font(22, wx.DECORATIVE, wx.NORMAL, wx.NORMAL)
+        titlefont = wx.Font(40, wx.DECORATIVE, wx.NORMAL, wx.NORMAL)
         title.SetForegroundColour(wx.BLACK)
         title.SetFont(titlefont)
+
+        smallfont = wx.Font(20, wx.DECORATIVE, wx.NORMAL, wx.NORMAL)
+
         # username
         nameBox = wx.BoxSizer(wx.HORIZONTAL)
         nameText = wx.StaticText(self, 1, label="Username: ")
-        self.nameField = wx.TextCtrl(self, -1, name="username", size=(150, -1))
+        nameText.SetFont(smallfont)
+        self.nameField = wx.TextCtrl(self, -1, name="username", size=(300, 50))
+        self.nameField.SetFont(smallfont)
         nameBox.Add(nameText, 0, wx.ALL, 5)
         nameBox.Add(self.nameField, 0, wx.ALL, 5)
         # password
         passBox = wx.BoxSizer(wx.HORIZONTAL)
         passText = wx.StaticText(self, 1, label="Password: ")
-        self.passField = wx.TextCtrl(self, -1, name="password", style=wx.TE_PASSWORD, size=(150, -1))
+        passText.SetFont(smallfont)
+        self.passField = wx.TextCtrl(self, -1, name="password", style=wx.TE_PASSWORD, size=(300, 50))
+        self.passField.SetFont(smallfont)
         passBox.Add(passText, 0, wx.ALL, 5)
         passBox.Add(self.passField, 0,  wx.ALL, 5)
         # login & registration buttons
+        login = wx.Image("pics\\done.png")
+        login.Rescale(250, 100)
+        back = wx.Image("pics\\back.png")
+        back.Rescale(200, 80)
+        back = wx.Bitmap(back)
+        login = wx.Bitmap(login)
+        logBtn = wx.BitmapButton(self,
+                                 size=(200, 80), name="button", bitmap=login)
+        logBtn.SetBackgroundColour(wx.LIGHT_GREY)
+        logBtn.SetWindowStyleFlag(wx.NO_BORDER)
+        backBtn = wx.BitmapButton(self,
+                                  size=(200, 80), name="button", bitmap=back)
+        backBtn.SetBackgroundColour(wx.LIGHT_GREY)
+        backBtn.SetWindowStyleFlag(wx.NO_BORDER)
+
         btnBox = wx.BoxSizer(wx.HORIZONTAL)
-        backBtn = wx.Button(self, wx.ID_ANY, label="Back", size=(100, 40))
+        # backBtn = wx.Button(self, wx.ID_ANY, label="Back", size=(200, 80))
         backBtn.Bind(wx.EVT_BUTTON, self.handle_back)
         btnBox.Add(backBtn, 1, wx.ALL, 5)
-        loginBtn = wx.Button(self, wx.ID_ANY, label="login", size=(100, 40))
-        loginBtn.Bind(wx.EVT_BUTTON, self.handle_login)
-        btnBox.Add(loginBtn, 0, wx.ALL, 5)
+        btnBox.AddSpacer(40)
+        # registerBtn = wx.Button(self, wx.ID_ANY, label="register", size=(200, 80))
+        logBtn.Bind(wx.EVT_BUTTON, self.handle_login)
+        btnBox.Add(logBtn, 0, wx.ALL, 5)
+        # btnBox = wx.BoxSizer(wx.HORIZONTAL)
+        # backBtn = wx.Button(self, wx.ID_ANY, label="Back", size=(200, 80))
+        # backBtn.Bind(wx.EVT_BUTTON, self.handle_back)
+        # btnBox.Add(backBtn, 1, wx.ALL, 5)
+        # btnBox.AddSpacer(40)
+        # loginBtn = wx.Button(self, wx.ID_ANY, label="login", size=(200, 80))
+        # loginBtn.Bind(wx.EVT_BUTTON, self.handle_login)
+        # btnBox.Add(loginBtn, 0, wx.ALL, 5)
         # add all elements to sizer
+        sizer.AddSpacer(40)
         sizer.Add(title, 0, wx.CENTER | wx.TOP, 5)
-        sizer.AddSpacer(10)
+        sizer.AddSpacer(60)
         sizer.Add(nameBox,0, wx.CENTER | wx.ALL, 5)
+        sizer.AddSpacer(20)
         sizer.Add(passBox, -1, wx.CENTER | wx.ALL, 5)
-        sizer.AddSpacer(10)
+        sizer.AddSpacer(30)
         sizer.Add(btnBox, wx.CENTER | wx.ALL, 5)
 
 
@@ -164,21 +197,42 @@ class RegistrationPanel(wx.Panel):
         phoneBox.Add(phoneText, 0, wx.ALL, 5)
         phoneBox.Add(self.phoneField, 0,  wx.ALL, 5)
         # login & registration buttons
+        register = wx.Image("pics\\done.png")
+        register.Rescale(250, 100)
+        back = wx.Image("pics\\back.png")
+        back.Rescale(200, 80)
+        back = wx.Bitmap(back)
+        register = wx.Bitmap(register)
+        regBtn = wx.BitmapButton(self,
+                                       size=(200, 80),  name="button", bitmap=register)
+        regBtn.SetBackgroundColour(wx.LIGHT_GREY)
+        regBtn.SetWindowStyleFlag(wx.NO_BORDER)
+        backBtn = wx.BitmapButton(self,
+                                        size=(200, 80),  name="button", bitmap=back)
+        backBtn.SetBackgroundColour(wx.LIGHT_GREY)
+        backBtn.SetWindowStyleFlag(wx.NO_BORDER)
+
+
         btnBox = wx.BoxSizer(wx.HORIZONTAL)
-        backBtn = wx.Button(self, wx.ID_ANY, label="Back", size=(200, 80))
+        # backBtn = wx.Button(self, wx.ID_ANY, label="Back", size=(200, 80))
         backBtn.Bind(wx.EVT_BUTTON, self.handle_back)
         btnBox.Add(backBtn, 1, wx.ALL, 5)
         btnBox.AddSpacer(40)
-        registerBtn = wx.Button(self, wx.ID_ANY, label="register", size=(200, 80))
-        registerBtn.Bind(wx.EVT_BUTTON, self.handle_registration)
-        btnBox.Add(registerBtn, 0, wx.ALL, 5)
+        # registerBtn = wx.Button(self, wx.ID_ANY, label="register", size=(200, 80))
+        regBtn.Bind(wx.EVT_BUTTON, self.handle_registration)
+        btnBox.Add(regBtn, 0, wx.ALL, 5)
         # add all elements to sizer
+        sizer.AddSpacer(40)
         sizer.Add(title, 0, wx.CENTER | wx.TOP, 5)
-        sizer.AddSpacer(10)
+        sizer.AddSpacer(60)
         sizer.Add(nameBox, 0, wx.CENTER | wx.ALL, 5)
+        sizer.AddSpacer(20)
+
         sizer.Add(passBox, -1, wx.CENTER | wx.ALL, 5)
+        sizer.AddSpacer(20)
+
         sizer.Add(phoneBox, -1, wx.CENTER | wx.ALL, 5)
-        sizer.AddSpacer(10)
+        sizer.AddSpacer(30)
         sizer.Add(btnBox, wx.CENTER | wx.ALL, 5)
 
 
@@ -216,8 +270,10 @@ class FirstPanel(wx.Panel):
         title.SetForegroundColour(wx.BLACK)
         title.SetFont(titlefont)
         # login & registration buttons
+        btnfont = wx.Font(20, wx.DECORATIVE, wx.NORMAL, wx.NORMAL)
         btnBox = wx.BoxSizer(wx.HORIZONTAL)
         loginBtn = wx.Button(self, wx.ID_ANY, label="login", size=(300, 300))
+        loginBtn.SetFont(btnfont)
         loginBtn.Bind(wx.EVT_BUTTON, self.handle_login)
         btnBox.Add(loginBtn, 0, wx.ALL, 5)
         btnBox.AddSpacer(50)
@@ -227,9 +283,10 @@ class FirstPanel(wx.Panel):
         sizer.AddSpacer(100)
         sizer.Add(btnBox, wx.CENTER | wx.ALL, 5)
 
-        backBtn = wx.Button(self, wx.ID_ANY, label="registeration", size=(300, 300))
-        backBtn.Bind(wx.EVT_BUTTON, self.handle_reg)
-        btnBox.Add(backBtn, 1, wx.ALL, 5)
+        regBtn = wx.Button(self, wx.ID_ANY, label="register", size=(300, 300))
+        regBtn.SetFont(btnfont)
+        regBtn.Bind(wx.EVT_BUTTON, self.handle_reg)
+        btnBox.Add(regBtn, 1, wx.ALL, 5)
 
         # arrange the screen
         self.SetSizer(sizer)
@@ -278,7 +335,7 @@ class CalendarPanel(wx.Panel):
         self.leftBtn.SetBackgroundColour(wx.LIGHT_GREY)
         self.leftBtn.SetWindowStyleFlag(wx.NO_BORDER)
         self.rightBtn = wx.BitmapButton(self,
-                                       size=(100, 80), pos=(700, 50), name="button", bitmap=right)
+                                       size=(100, 80), pos=(660, 50), name="button", bitmap=right)
         self.rightBtn.SetBackgroundColour(wx.LIGHT_GREY)
         self.rightBtn.SetWindowStyleFlag(wx.NO_BORDER)
         self.leftBtn.Bind(wx.EVT_BUTTON, self.left_cal)
@@ -289,10 +346,18 @@ class CalendarPanel(wx.Panel):
         #self.rightBtn.SetBitmap(right)
         title_row.Add(self.leftBtn, 1, wx.ALL, 5)
         title_row.Add(self.rightBtn, 1, wx.ALL, 5)
+        btnBox2 = wx.BoxSizer(wx.HORIZONTAL)
+        changeBtn = wx.Button(self, wx.ID_ANY, label="change name", size=(100, 40), pos=(765,95))
+        changeBtn.Bind(wx.EVT_BUTTON, self.change_calendar_name)
+        btnBox2.Add(changeBtn, 0, wx.ALL, 5)
+        addBtn = wx.Button(self, wx.ID_ANY, label="add participant", size=(100, 40), pos=(765,50))
+        addBtn.Bind(wx.EVT_BUTTON, self.add_calendar_participant)
+        btnBox2.Add(addBtn, 0, wx.ALL, 5)
 
 
 
         title_row.Add(title, 1, wx.ALL, 5)
+        title_row.Add(btnBox2, 0, wx.ALL, 5)
 
 
 
@@ -434,6 +499,13 @@ class CalendarPanel(wx.Panel):
     def right_cal(self, evt):
         print("scroll to right cal")
 
+    def change_calendar_name(self, evt):
+        print("try to change calendar name")
+
+    def add_calendar_participant(self, evt):
+        print("try to add participant to calendar")
+
+
 
 
 class ParticipantsPanel(wx.Panel):
@@ -488,7 +560,7 @@ class ParticipantsPanel(wx.Panel):
 
 
     def close(self, evt):
-        print("in close)")
+        print("in close")
         self.Hide()
         self.participantsBTN.Show()
 
